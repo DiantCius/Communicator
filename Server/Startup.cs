3e7a5aeeaf63f1f35d91e590dad3759168bb2437
misio <kartofel6223@gmail.com>
2022-05-13 13:19:14 +0200

    xd

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -46,22 +46,28 @@ namespace Server
             //    options.UseSqlServer(Configuration.GetConnectionString("Default")));
             var defaultConnectionString = string.Empty;
 
-            
-            // Use connection string provided at runtime by Heroku.
-            var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-            Console.WriteLine(connectionUrl+"dupa");
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            {
+                defaultConnectionString = Configuration.GetConnectionString("Default");
+            }
+            else
+            {
+                // Use connection string provided at runtime by Heroku.
+                var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-            connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
-            var userPassSide = connectionUrl.Split("@")[0];
-            var hostSide = connectionUrl.Split("@")[1];
 
-            var user = userPassSide.Split(":")[0];
-            var pwd = userPassSide.Split(":")[1];
-            var host = hostSide.Split("/")[0];
-            var database = hostSide.Split("/")[1].Split("?")[0];
+                connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+                var userPassSide = connectionUrl.Split("@")[0];
+                var hostSide = connectionUrl.Split("@")[1];
 
-            defaultConnectionString = $"Host={host};Database={database};Username={user};Password={pwd};SSL Mode=Require;Trust Server Certificate=true";
-            
+                var user = userPassSide.Split(":")[0];
+                var pwd = userPassSide.Split(":")[1];
+                var host = hostSide.Split("/")[0];
+                var database = hostSide.Split("/")[1].Split("?")[0];
+
+                defaultConnectionString = $"Host={host};Database={database};Username={user};Password={pwd};SSL Mode=Require;Trust Server Certificate=true";
+
+            }
             
             services.AddDbContext<ApplicationContext>(options =>
                options.UseNpgsql(defaultConnectionString));
